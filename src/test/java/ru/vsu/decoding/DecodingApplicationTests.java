28aadf0dec4ec6696f5da54496edc5effe534655
vladimir <grigorevvladimir1999@gmail.com>
2020-10-05 21:50:45 +0300

    Initial commit

---
+++ src/test/java/ru/vsu/decoding/DecodingApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vsu.decoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
